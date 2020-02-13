ca2055802c30e29447aae9797674ad02a6336d2b
Ashay Aswale <asaswale@wpi.edu>
2020-02-12 22:20:59 -0500

    Code Commenting

--- include/ros_tutorial_src/robot_controller.h
+++ include/ros_tutorial_src/robot_controller.h
@@ -24,26 +24,63 @@ private:
   ros::Publisher velocity_pub_;
   ros::Subscriber odom_sub_, goal_sub_;
 
+  /**
+   * @brief Calculates the length of a vector.
+   * 
+   * @param x   [float] The X coordinate.
+   * @param y   [float] The Y coordinate.
+   * @return float      The length.
+   */
   inline float calcVecLength(const float x, const float y)
   {
     return std::sqrt(x * x + y * y);
   }
 
+  /**
+   * @brief Calculates the angle of a vector in radians normalized between -pi and pi.
+   * 
+   * @param x    [float] The X coordinate.
+   * @param y    [float] The Y coordinate.
+   * @return float [rad] The angle.
+   */
   inline float calcVecAngle(const float x, const float y)
   {
     return std::atan2(y, x);
   }
 
+  /**
+   * @brief Checks whether a certain value is within a given tolerance.
+   *        The passed value can be any sign. Internally, the check is
+   *        performed on the absolute value of x.
+   * 
+   * @param value     [float] The value to check.
+   * @return true     True if within tolerance, False otherwise
+   * @return false 
+   */
   inline bool isWithinTolerance(const float value)
   {
     return (std::fabs(value))<DISTANCE_TOLERANCE;
   }
 
+  /**
+   * @brief Converts input msg to return Yaw
+   * 
+   * @param msg_quat  [geometry_msgs::Quaternion] Input quaternion msg 
+   * @return float    Yaw
+   */
   inline float convQuatToEuler(const geometry_msgs::Quaternion &msg_quat)
   {
     return tf::getYaw(msg_quat);
   }
 
+  /**
+   * @brief Checks if the robot has covered the distance
+   * 
+   * @param init_pose     [geometry_msgs::Point] Initial position of the robot 
+   * @param distance      [float] Distance to be covered
+   * @return true         When robot has covered the distance.
+   * @return false 
+   */
   inline bool isReached(const geometry_msgs::Point &init_pose, const float distance)
   {
     float travel_dist = calcVecLength(init_pose.x - robot_curr_x_, init_pose.y - robot_curr_y_);
@@ -51,20 +88,96 @@ private:
     return travel_dist < desired_travel_dist;
   }
 
+  /**
+   * @brief Updates the current pose of the robot.
+   *        This method is a callback bound to a Subscriber.
+   * 
+   * @param msg [Odometry] The current odometry information.
+   */
   void odometryCB(const nav_msgs::Odometry &msg);
+  
+  /**
+   * @brief Updates the Goal Poisition for robot
+   *        This method is a callback bound to a Subscriber.
+   * 
+   * @param msg [PoseStamped] The target pose.
+   */
   void goalCB(const geometry_msgs::PoseStamped &msg);
 
+  /**
+   * @brief  Normalizes the given angle between -pi and pi.
+   * 
+   * @param value     [float] [rad] The angle.
+   * @return float    [float] [rad] The normalized value.
+   */
   float normalizeAngle(const float value);
+  
+  /**
+   * @brief Calculates the difference between two angles.
+   *        The return value is normalized between -pi and pi.
+   * 
+   * @param ang_1   [float] [rad] The first angle.
+   * @param ang_2   [float] [rad] The second angle.
+   * @return float  [float] [rad] The difference between the angles.
+   */
   float calcAngleDiff(const float ang_1, const float ang_2);
-  void driveRobot(const float distanec, const float linear_velocity);
+  
+  /**
+   * @brief  Drives the robot in a straight line.
+   * 
+   * @param distance         [float] [m]   The distance to cover.
+   * @param linear_velocity  [float] [m/s] The forward linear speed.
+   */
+  void driveRobot(const float distance, const float linear_velocity);
+
+  /**
+   * @brief Drives the robot in a straight line by changing the actual speed smoothly.
+   * 
+   * @param distance         [float] [m]   The distance to cover.
+   * @param linear_velocity  [float] [m/s] The maximum forward linear speed.
+   */
   void smoothDriveRobot(const float distance, const float linear_velocity);
 
+
+
 public:
+  
+  /**
+   * @brief Construct a new Robot Controller:: Robot Controller object
+   * 
+   * @param nh 
+   */
   RobotController(ros::NodeHandle nh);
+
+  /**
+   * @brief Destroy the Robot Controller:: Robot Controller object
+   *        Delete pointers here, if used
+   * 
+   */
   ~RobotController();
-  void runController();
+  
+  // Set true when goal is available on topic: /move_base_simple/goal
   bool goal_available_ = false;
+  
+  /**
+   * @brief Sends the speeds to the motors.
+   * 
+   * @param linear_vel  [float] [m/s]   The forward linear speed.
+   * @param angular_vel [float] [rad/s] The angular speed for rotating around the body center.
+   */
   void publishVelocity(const float linear_vel, const float angular_vel);
+  
+  /**
+   * @brief Rotates the robot around the body center by the given angle.
+   * 
+   * @param angle             [float] [rad]   The distance to cover.
+   * @param angular_velocity  [float] [rad/s] The angular speed.
+   */
   void rotateRobot(const float angle, const float angular_velocity);
+  
+  /**
+   * @brief Calls rotate(), drive(), and rotate() to attain a given pose.
+   * 
+   */
   void goToGoal();
 };
--- src/robot_controller.cpp
+++ src/robot_controller.cpp
@@ -2,8 +2,15 @@
 
 RobotController::RobotController(ros::NodeHandle nh) : nh_(nh)
 {
+  // ### Tell ROS that this node publishes Twist messages on the '/cmd_vel' topic
   velocity_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+  
+  // ### Tell ROS that this node subscribes to Odometry messages on the '/odom' topic
+  // ### When a message is received, call RobotController::odometryCB
   odom_sub_ = nh_.subscribe("/odom", 1, &RobotController::odometryCB, this);
+  
+  // ### Tell ROS that this node subscribes to PoseStamped messages on the '/move_base_simple/goal' topic
+  // ### When a message is received, call RobotController::goalCB
   goal_sub_ = nh_.subscribe("/move_base_simple/goal", 1, &RobotController::goalCB, this);
 }
 
@@ -29,13 +36,17 @@ void RobotController::goalCB(const geometry_msgs::PoseStamped &msg)
 
 void RobotController::publishVelocity(const float linear_vel, const float angular_vel)
 {
+  // # Create twist message
   geometry_msgs::Twist robot_twist;
+  // # Linear velocity
   robot_twist.linear.x = linear_vel;
   robot_twist.linear.y = 0.0;
   robot_twist.linear.z = 0.0;
+  // # Angular velocity
   robot_twist.angular.x = 0.0;
   robot_twist.angular.y = 0.0;
   robot_twist.angular.z = angular_vel;
+  // # Send command
   velocity_pub_.publish(robot_twist);
 }
 
@@ -60,50 +71,65 @@ float RobotController::calcAngleDiff(const float ang_1, const float ang_2)
 
 void RobotController::driveRobot(const float distance, const float linear_velocity)
 {
+  //  # Save current position
   geometry_msgs::Point init_position;
   init_position.x = robot_curr_x_;
   init_position.y = robot_curr_y_;
-
+  
+  // # Go straight
   publishVelocity(linear_velocity, 0.0);
 
+  // # Keep going until the distance is not within the tolerance
   while (isReached(init_position, distance))
     ros::Duration(SLEEP_TIME).sleep();
 
+  // # Stop
   publishVelocity(0.0, 0.0);
 }
 
 void RobotController::rotateRobot(const float angle, const float angular_velocity)
 {
+  // # Calculate limit angle after which we must stop
   float limit_angle = calcAngleDiff(angle, ANGLE_TOLERANCE);
+
+  // # Save the initial orientation
   float init_th = robot_curr_th_;
 
+  // # Rotate
   publishVelocity(0.0, std::copysign(angular_velocity, angle));
 
+  // # Keep going until we're within angle tolerance
   while (std::fabs(calcAngleDiff(robot_curr_th_, init_th)) < std::fabs(limit_angle))
     ros::Duration(SLEEP_TIME).sleep();
 
+  // # Stop
   publishVelocity(0.0, 0.0);
 }
 
 void RobotController::goToGoal()
 {
+  // Set variable false to enable resetting goal position during run.
   goal_available_ = false;
 
+  // # Save target pose
   float goal_x = goal_pose_.pose.position.x;
   float goal_y = goal_pose_.pose.position.y;
   geometry_msgs::Quaternion goal_quat = goal_pose_.pose.orientation;
   float goal_th = tf::getYaw(goal_quat);
 
+  // # Rotate to face target
   ROS_INFO("Facing direction.");
   float vec_angle = calcVecAngle(goal_x - robot_curr_x_, goal_y - robot_curr_y_);
   float face_th = calcAngleDiff(vec_angle, robot_curr_th_);
   rotateRobot(face_th, ANGULAR_SPEED);
 
+  // # Drive straight to target
   ROS_INFO("Driving Robot.");
   float dist = calcVecLength(goal_x - robot_curr_x_, goal_y - robot_curr_y_);
-  // driveRobot(dist, LINEAR_SPEED);
-  smoothDriveRobot(dist, LINEAR_SPEED);
+  driveRobot(dist, LINEAR_SPEED);
+  // smoothDriveRobot(dist, LINEAR_SPEED);
 
+  // # Rotate to final pose
   ROS_INFO("Orienting Robot");
   float final_th = calcAngleDiff(goal_th, robot_curr_th_);
   rotateRobot(final_th, ANGULAR_SPEED);
@@ -113,9 +139,16 @@ void RobotController::goToGoal()
 
 void RobotController::smoothDriveRobot(const float distance, const float linear_velocity)
 {
+  // # Save initial position and time
   geometry_msgs::Point init_position;
   init_position.x = robot_curr_x_;
   init_position.y = robot_curr_y_;
+
+  // # Fix the length of phases 1 and 3 to 20% of the total drive distance
+  //       # Phase 0 is when we're the farthest
+  //       # Phase 1 is when we drive at max speed
+  //       # Phase 2 is when we're almost at the target
+  //       # Store end of each phase
 
   std::vector<float> phases;
   phases.resize(3);
@@ -124,61 +157,78 @@ void RobotController::smoothDriveRobot(const float distance, const float linear_
   phases.at(2) = distance*0.2;
 
   bool reached = false;
+
+  // # Phase 1: ramp speed up
   while (!reached)
   {
+    // # Get current distance
     float vec_length = calcVecLength(init_position.x - robot_curr_x_, init_position.y - robot_curr_y_);
     float curr_dist = std::fabs(distance - vec_length);
 
+    // # Identify in which phase we are
     int phase = 0;
     if(curr_dist < phases.at(2))
       phase = 2;
     else if (curr_dist < phases.at(1))
       phase = 1;
 
+    // # Check if we arrived to destination
     if(isWithinTolerance(curr_dist))
     {
       reached = true;
       break;
     }
 
+    // # Calculate the linear speed depending on the phase
     float ls = linear_velocity;
     if (phase == 0)
       ls = linear_velocity * (1.0 - (curr_dist - phases.at(1)) / (phases.at(2) - phases.at(1)));
     else if(phase == 2)
       ls = linear_velocity * ((curr_dist + 0.01) / phases.at(2));
 
+    // # Set speed
     publishVelocity(ls, 0.0);
+
+    // # Sleep a little
     ros::Duration(SLEEP_TIME).sleep();
   }
+
+  // # Stop robot
   publishVelocity(0.0, 0.0);
 }
 
 int main(int argc, char **argv)
 {
+  // ### Initialize node, name it 'robot_controller'
   ros::init(argc, argv, "robot_controller");
+
+  // # Create Nodehandle 
   ros::NodeHandle nh;
+
   ros::Rate rate(500);
+  // #spinners are needed for subscribers
   ros::AsyncSpinner spinner(1);
 
   spinner.start();
+
+  // # Create object of controller class.
   RobotController control(nh);
   ros::Duration(1.0).sleep();
 
-  // control.rotateRobot(M_PI / 4, 0.5);
-  // ros::Duration(1.0).sleep();
-
   while (ros::ok())
-  {
-    if (control.goal_available_)
-    {
-      ROS_INFO("Moving to Goal");
-      control.goToGoal();
-    }
-    // ROS_INFO("Sleeping...");
-    rate.sleep();
+ {
+  //  # Check if goal has published.
+   if (control.goal_available_)
+   {
+    //  # if goal is available, move the robot
+     ROS_INFO("Moving to Goal");
+     control.goToGoal();
+   }
+  //  # Sleep for some time
+   rate.sleep();
   }
 
-  ros::Duration(1.0).sleep();
   spinner.stop();
+  ros::Duration(1.0).sleep();
   return 0;
 }
